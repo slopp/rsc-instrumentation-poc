b735078504dfe75606dc728779eaeb4c81e698b4
slopp <sean@rstudio.com>
2018-10-22 09:48:50 -0600

    stash some changes

--- helpers.R
+++ helpers.R
@@ -4,16 +4,12 @@ library(magrittr)
 lookup_user <- function(id, type = "guid") {
   stopifnot(type %in% c("guid", "username"))
   
-  # special case for anonymous users
-  if (id == "anonymous") {
+  # this will need to be updated based on the results returned by the usage API
+  if (is.null(id)) {
     return("anonymous-guid")
   }
   
-  if (id == "anonymous-guid") {
-    return("anonymous")
-  }
   
-  # shortcut for 
   # this will get better when the user API supports search
   # for now, we cache the user list for 10 minutes
   # TODO: This does not recover well if get_all_users fails
@@ -75,7 +71,7 @@ get_all_users <- function(){
 }
 
 # get usage data for an app, optionally filtering by content GUID
-get_shiny_usage_new <- function(content_guid = NA) {
+get_shiny_usage <- function(content_guid = NA) {
   connectServer <- Sys.getenv("RSTUDIO_CONNECT_SERVER")
   apiKey <- Sys.getenv("RSTUDIO_CONNECT_API_KEY")
   
@@ -106,7 +102,7 @@ get_shiny_usage_new <- function(content_guid = NA) {
                                             user_guid = .x$user_guid,
                                             ended = .x$ended)))
   
-  # now step through the remaining audit logs
+  # now step through the remaining pages
   while (!is.null(payload$paging[["next"]])) {
     resp <- httr::GET(payload$paging[["next"]], authHeader)
     payload <- httr::content(resp)
@@ -120,35 +116,33 @@ get_shiny_usage_new <- function(content_guid = NA) {
                                               ended = .x$ended)))
   }
   
-  
-  #temporary till the GUID migration lands
-  add_user_guid_to_usage(results)
+  result  
 }
 
 # fake implementation
-get_shiny_usage <- function(content_guid = NA) {
-  con <- odbc::dbConnect(RSQLite::SQLite(), "connect-instrumentation.db")
-  on.exit(dbDisconnect(con))
-  usage_data <- dbReadTable(con, "shiny_app_usage")
-  if (!is.na(content_guid)) {
-    usage_data <- usage_data[which(usage_data$app_id == content_guid),]
-  }
-  add_user_guid_to_usage(usage_data)
-}
-
-# the instrumentation db has user ids... but the api has user guids. sigh
-# temporary patch
-add_user_guid_to_usage <- function(usage_data) {
-  id_table <- readr::read_csv("guid-id-map.csv")
-  usage_data <- usage_data %>% 
-    dplyr::left_join(id_table, by = c("user_id" = "id"))
-  usage_data$username <- ifelse(usage_data$user_id==0 , "anonymous", usage_data$username)
-  
-  usage_data$user_guid <- purrr::map_chr(usage_data$username, ~lookup_user(.x, "username"))
-  
-  # now no cheating!
-  usage_data$username <- NULL
-  usage_data
-}
-
-  
+# get_shiny_usage <- function(content_guid = NA) {
+#   con <- odbc::dbConnect(RSQLite::SQLite(), "connect-instrumentation.db")
+#   on.exit(dbDisconnect(con))
+#   usage_data <- dbReadTable(con, "shiny_app_usage")
+#   if (!is.na(content_guid)) {
+#     usage_data <- usage_data[which(usage_data$app_id == content_guid),]
+#   }
+#   add_user_guid_to_usage(usage_data)
+# }
+# 
+# # the instrumentation db has user ids... but the api has user guids. sigh
+# # temporary patch
+# add_user_guid_to_usage <- function(usage_data) {
+#   id_table <- readr::read_csv("guid-id-map.csv")
+#   usage_data <- usage_data %>% 
+#     dplyr::left_join(id_table, by = c("user_id" = "id"))
+#   usage_data$username <- ifelse(usage_data$user_id==0 , "anonymous", usage_data$username)
+#   
+#   usage_data$user_guid <- purrr::map_chr(usage_data$username, ~lookup_user(.x, "username"))
+#   
+#   # now no cheating!
+#   usage_data$username <- NULL
+#   usage_data
+# }
+# 
+#   
